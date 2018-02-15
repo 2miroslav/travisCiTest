b5a07427ede885bb8533d410d6d696ef1ccdb913
2miroslav <2miroslavrandlisek@gmail.com>
2018-02-15 19:24:45 +0100

    last set3223sss

--- tests/HelloTest.php
+++ tests/HelloTest.php
@@ -1,9 +1,11 @@
 <?php
+
 class HelloTest extends PHPUnit_Framework_TestCase{
 	protected $hello;
 	public function setUp(){
 		$this->hello = new \Dracony\Demo\Hello();
 	}
+
 	public function testWorld(){
 		$this->assertSame('world',$this->hello->world());
 	}
