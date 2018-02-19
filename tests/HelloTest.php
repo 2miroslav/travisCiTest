5c849ce5a1fde6138ffe241896e7658f4910d923
Miroslav <2miroslavrandlisek@gmail.com>
2018-02-19 22:19:33 +0100

    Update HelloTest.php
    delete covers

--- tests/HelloTest.php
+++ tests/HelloTest.php
@@ -11,9 +11,7 @@ class HelloTest extends PHPUnit_Framework_TestCase{
 	public function testCalculator(){
 		$this->assertEquals(5,$this->hello->calculator(3,2));
 	}
-	/**
-	* @covers Hello::returnTrue
-	*/
+	
 	public function testReturnTrue(){
 		$this->assertEquals(true,$this->hello->returnTrue());
 	}
