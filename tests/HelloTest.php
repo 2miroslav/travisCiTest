0dd69196bc7a83b348eb3da03f528080fa6daedd
Miroslav <2miroslavrandlisek@gmail.com>
2018-02-19 22:18:09 +0100

    Update HelloTest.php
    ss

--- tests/HelloTest.php
+++ tests/HelloTest.php
@@ -12,7 +12,7 @@ class HelloTest extends PHPUnit_Framework_TestCase{
 		$this->assertEquals(5,$this->hello->calculator(3,2));
 	}
 	/**
-	*@covers $this->hello::returnTrue
+	* @covers Hello::returnTrue
 	*/
 	public function testReturnTrue(){
 		$this->assertEquals(true,$this->hello->returnTrue());
